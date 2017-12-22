9ca81695fc7e04565e19fcf4524322e5e086810d
max <max.teh.lueser@gmail.com>
2017-12-22 16:06:56 +0000

    Check that twttr has been initialised before calling widgets.load()

--- src/js/messageList.js
+++ src/js/messageList.js
@@ -2191,7 +2191,7 @@ var messageList = {
 		
 		injectLoadScript: function(id) {
 			var script = document.createElement('script');
-			script.innerHTML = 'twttr.widgets.load(document.getElementById("' + id + '"));';
+			script.innerHTML = 'if (window.twttr) { twttr.widgets.load(document.getElementById("' + id + '")) };';
 			script.onload = function() {
 					this.remove();
 			};
